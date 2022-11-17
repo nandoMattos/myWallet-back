8cbcd70331af7ece13ba5f741155844e6f5d4369
Luiz Fernando Mattos <luizfernandoamattos@gmail.com>
2022-11-17 15:50:29 -0300

    feat: sign in

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,36 @@
 import userSchema from "../schemas/userSchema.js";
 import bcrypt from "bcrypt";
 import { COLLECTIONS } from "../index.js";
+import { v4 as uuidV4 } from "uuid";
 
 export async function signIn(req, res) {
   const { email, password } = req.body;
+
+  try {
+    const user = await COLLECTIONS.USERS.findOne({ email });
+    if (!user) {
+      res.status(404).send({ message: "Email não cadastrado." });
+      return;
+    }
+
+    if (!bcrypt.compareSync(password, user.password)) {
+      res.status(401).send({ message: "Senha inválida." });
+      return;
+    }
+
+    const session = await COLLECTIONS.SESSIONS.findOne({ userId: user._id });
+    if (session) {
+      res.status(409).send({ message: "Usuário já logado." });
+      return;
+    }
+
+    const token = uuidV4();
+    await COLLECTIONS.SESSIONS.insertOne({ userId: user._id, token });
+    res.status(200).send(token);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 }
 
 export async function signUp(req, res) {
@@ -16,20 +43,13 @@ export async function signUp(req, res) {
     return;
   }
 
-  // se já existe
   try {
     const findUser = await COLLECTIONS.USERS.findOne({ email: user.email });
     if (findUser) {
       res.status(409).send({ message: "Email já registrado." });
       return;
     }
-  } catch (err) {
-    console.log(err);
-    res.sendStatus(500);
-  }
-  //
 
-  try {
     await COLLECTIONS.USERS.insertOne({
       ...user,
       password: bcrypt.hashSync(user.password, 10),
